244f06088049f92a21cca8d6e14aa9d442caa6a1
Kris Hicks <khicks@pivotal.io>
2017-03-07 13:37:26 -0800

    Be noisy

--- ci/tasks/configure-ert.sh
+++ ci/tasks/configure-ert.sh
@@ -60,7 +60,9 @@ function fn_om_linux_curl {
   echo "om-linux ${args}" > /tmp/rqst_cmd.log
 
   set +e
-  om-linux "${args}" 1> /tmp/rqst_stdout.log 2> /tmp/rqst_stderr.log
+  set -x
+  om-linux ${args} 1> /tmp/rqst_stdout.log 2> /tmp/rqst_stderr.log
+  set +x
   if [ $? -ne 0 ]; then
     echo Failed:
     echo stdout:
