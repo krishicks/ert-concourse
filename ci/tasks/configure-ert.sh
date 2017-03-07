001fe7a5bd108b7334dbc2058ebd026e24b8aa68
Kris Hicks <khicks@pivotal.io>
2017-03-07 08:38:30 -0800

    Rejigger fn_om_linux_curl
    It previously didn't give a helpful error message when it fails; the
    contents of the stderr.log weren't printed out.

--- ci/tasks/configure-ert.sh
+++ ci/tasks/configure-ert.sh
@@ -40,36 +40,25 @@ if [[ ! -f ${json_file} ]]; then
 fi
 
 function fn_om_linux_curl {
+  local curl_method=${1}
+  local curl_path=${2}
+  local curl_data=${3}
 
-    local curl_method=${1}
-    local curl_path=${2}
-    local curl_data=${3}
+  args="--target https://opsman.$pcf_ert_domain -k \
+    --username \"$pcf_opsman_admin\" \
+    --password \"$pcf_opsman_admin_passwd\"  \
+    curl \
+    --request ${curl_method} \
+    --path ${curl_path}"
 
-     curl_cmd="om-linux --target https://opsman.$pcf_ert_domain -k \
-            --username \"$pcf_opsman_admin\" \
-            --password \"$pcf_opsman_admin_passwd\"  \
-            curl \
-            --request ${curl_method} \
-            --path ${curl_path}"
+  if [ -n ${curl_data} ]; then
+    args="${args} --data '${curl_data}'"
+  fi
 
-    if [[ ! -z ${curl_data} ]]; then
-       curl_cmd="${curl_cmd} \
-            --data '${curl_data}'"
-    fi
+  echo "om-linux ${args}" > /tmp/rqst_cmd.log
 
-    echo ${curl_cmd} > /tmp/rqst_cmd.log
-    exec_out=$(((eval $curl_cmd | tee /tmp/rqst_stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/rqst_stderr.log) &>/dev/null)
-
-    if [[ $(cat /tmp/rqst_stderr.log | grep "Status:" | awk '{print$2}') != "200" ]]; then
-      echo "Error Call Failed ...."
-      echo $(cat /tmp/rqst_stderr.log)
-      exit 1
-    else
-      echo $(cat /tmp/rqst_stdout.log)
-    fi
+  om-linux $args > >(tee /tmp/rqst_stdout.log) 2> >(tee /tmp/rqst_stderr.log >&2)
 }
-
-
 
 echo "=============================================================================================="
 echo "Deploying ERT @ https://opsman.$pcf_ert_domain ..."
